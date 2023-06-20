c6f75c7e84913162fe3ca5d2c8e2ba89cebe94e9
AliSot2000 <alisot200@gmail.com>
2023-06-20 20:09:11 +0200

    + Reduced number of workers + Taking measure of the total time for the script.

--- scripts/main.py
+++ scripts/main.py
@@ -5,11 +5,12 @@ from eth_loader.stream_loader import BetterStreamLoader, SpecLogin
 from secrets import user_name, password, spec_login
 
 if __name__ == "__main__":
+    global_start = datetime.datetime.now()
     path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites.db"
     index_start = datetime.datetime.now()
     print("Started")
     eid = ConcurrentETHSiteIndexer(path)
-    eid.index_video_eth()
+    eid.index_video_eth(threads=10)
     eid.gen_parent()
     end = datetime.datetime.now()
     print(f"required {(end - index_start).total_seconds()}s")
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     start = datetime.datetime.now()
     print("Started")
     eid = EpisodeLoader(path)
-    eid.download()
+    eid.download(10)
     eid.deprecate(dt=index_start)
     end = datetime.datetime.now()
     print(f"required {(end - start).total_seconds()}s")
@@ -25,6 +26,8 @@ if __name__ == "__main__":
     start = datetime.datetime.now()
     print("Started")
     eid = BetterStreamLoader(db=path, user_name=user_name, password=password, spec_login=spec_login)
-    eid.initiator()
+    eid.initiator(workers=10)
     end = datetime.datetime.now()
     print(f"required {(end - start).total_seconds()}s")
+    global_end = datetime.datetime.now()
+    print(f"Overall Time for complete indexing: {(global_end - global_start).total_seconds()}")
