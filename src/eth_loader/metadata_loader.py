bc1ed3e692f12e6d437b79673f5125c2716f9f4e
AliSot2000 <alisot200@gmail.com>
2023-12-16 15:29:11 +0100

    + Switching momentarely to json again

--- src/eth_loader/metadata_loader.py
+++ src/eth_loader/metadata_loader.py
@@ -240,7 +240,10 @@ class EpisodeLoader(BaseSQliteDB):
                     url = res["url"]
 
                     if res["status"] == 200:
-                        content = aux.to_b64(res["content"])
+                        # content = aux.to_b64(res["content"])
+                        if "'" in res["content"]:
+                            print(res["content"])
+                        content = res["content"].replace("'", "''")
                         self.insert_update_db(parent_id=parent_id, url=url, json_arg=content)
                     else:
                         self.logger.error(f"Failed to download {url} with status code {res['status']}")
