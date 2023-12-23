714d1dd804f1ac7686054dd37f59b7e07a7712ad
AliSot2000 <alisot200@gmail.com>
2023-12-23 18:58:15 +0100

    + Updated for new interface

--- scripts/main.py
+++ scripts/main.py
@@ -6,31 +6,46 @@ from secrets import user_name, password, spec_login
 from logs import setup_logging
 
 
-if __name__ == "__main__":
-    setup_logging(default_path="logging_production.yaml")
-    global_start = datetime.datetime.now()
-    path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites.db"
-    index_start = datetime.datetime.now()
+def perform_index_of_sites(db: str):
     print("Started")
-    eid = ConcurrentETHSiteIndexer(path)
+    index_start = datetime.datetime.now()
+    eid = ConcurrentETHSiteIndexer(db)
     eid.index_video_eth(threads=10)
     eid.gen_parent()
     end = datetime.datetime.now()
     print(f"required {(end - index_start).total_seconds()}s")
 
+
+def download_all_metadata(db, index_start: datetime.datetime, b64: bool = False):
     start = datetime.datetime.now()
     print("Started")
-    eid = EpisodeLoader(path)
-    eid.download(10)
+    print(index_start)
+    eid = EpisodeLoader(db, use_base64=b64)
+    eid.download(index_start, 10)
     eid.deprecate(dt=index_start)
     end = datetime.datetime.now()
     print(f"required {(end - start).total_seconds()}s")
 
+
+def download_all_stream_data(db: str, index_start: datetime.datetime, b64: bool = False):
     start = datetime.datetime.now()
     print("Started")
-    eid = BetterStreamLoader(db=path, user_name=user_name, password=password, spec_login=spec_login)
-    eid.initiator(workers=10)
+    bsl = BetterStreamLoader(db=db, user_name=user_name, password=password, spec_login=spec_login, use_base64=b64)
+    bsl.initiator(workers=10)
+    bsl.deprecate(index_start)
     end = datetime.datetime.now()
     print(f"required {(end - start).total_seconds()}s")
+
+
+if __name__ == "__main__":
+    # setup_logging(default_path="logging_debug.yaml")
+    setup_logging(default_path="logging_production.yaml")
+    global_start = datetime.datetime.now()
+    # path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites_b64.db"
+    path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites.db"
+    perform_index_of_sites(path)
+    download_all_metadata(path, global_start, b64=False)
+    download_all_stream_data(path, global_start, b64=False)
+
     global_end = datetime.datetime.now()
-    print(f"Overall Time for complete indexing: {(global_end - global_start).total_seconds()}")
+    print(f"Overall Time for complete indexing: {(global_end - global_start).total_seconds():.02f}")
