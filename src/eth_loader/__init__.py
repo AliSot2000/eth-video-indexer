cd0cefa16bc5e54c44d70c3026a30b732315379f
AliSot2000 <alisot200@gmail.com>
2024-05-04 00:57:53 +0200

    + Removed files that are acitvely used

--- src/eth_loader/__init__.py
+++
@@ -1,8 +0,0 @@
-pass
-# import indexer
-# import concurrent_indexer
-# import MetaDataLoader
-
-# TODOs
-# TODO IDEA: keep list of last seen for recurrent indexing? (Site indexer)
-# TODO Make all b64
