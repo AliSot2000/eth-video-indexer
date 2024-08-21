5cb2c46fd35aa003fd784dcb4fac391b8a26dafe
AliSot2000 <alisot200@gmail.com>
2024-08-21 18:56:04 +0200

    + Changed paths

--- src/eth_loader/remove_duplicated_middle.py
+++ src/eth_loader/remove_duplicated_middle.py
@@ -286,8 +286,8 @@ class MiddlePruner(BaseSQliteDB):
 
 # TODO check metadata for duplicates and ignore the lower end.
 if __name__ == "__main__":
-    path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites.db"
-    # path = "/home/alisot2000/Documents/01_ReposNCode/ETH-Lecture-Indexer/scripts/seq_sites_b64.db"
+    path = "/home/alisot2000/Documents/01_ReposNCode/eth-video-indexer/scripts/seq_sites.db"
+    # path = "/home/alisot2000/Documents/01_ReposNCode/eth-video-indexer/scripts/seq_sites_b64.db"
     obj = MiddlePruner(path)
     obj.create_hash_table()
     obj.perform_cleaning_episodes()
