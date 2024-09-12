f96184947e638c1daf8b2d4336594a327d32179b
AliSot2000 <alisot200@gmail.com>
2024-09-12 22:32:48 +0200

    + Removed TODO - not necessary

--- src/eth_loader/__init__.py
+++ src/eth_loader/__init__.py
@@ -2,7 +2,3 @@ pass
 # import indexer
 # import concurrent_indexer
 # import MetaDataLoader
-
-# TODOs
-# TODO IDEA: keep list of last seen for recurrent indexing? (Site indexer)
-# TODO Make all b64
