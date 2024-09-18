5585d5ad7fbfb96589e75185ac7030dbdb2351b9
AliSot2000 <alisot200@gmail.com>
2024-09-18 20:53:19 +0200

    + More stuff for testing

--- scripts/b64.sql
+++ scripts/b64.sql
@@ -58,3 +58,48 @@ UPDATE metadata SET deprecated = 0 WHERE DATETIME(last_seen) >= datetime('2024-0
 SELECT * FROM episodes GROUP BY DATE(found);
 
 SELECT * FROM sqlite_master;
+
+SELECT found FROM metadata ORDER BY found DESC LIMIT 5;
+
+SELECT * FROM metadata WHERE URL IN (SELECT URL FROM metadata WHERE DATETIME(found) >= DATETIME('2024-09-10 00:00:00')) ORDER BY parent;
+SELECT * FROM metadata WHERE URL IN (SELECT URL FROM metadata WHERE DATETIME(found) >= DATETIME('2024-08-24 00:00:00')) ORDER BY parent;
+
+SELECT * FROM metadata WHERE URL = 'https://www.video.ethz.ch/speakers/introductory_lectures.series-metadata.json';
+
+SELECT URL, parent FROM metadata WHERE deprecated = 0 AND key NOT IN (SELECT key FROM temp);
+
+SELECT * FROM metadata WHERE URL = 'https://www.video.ethz.ch/lectures/d-baug/2021/spring/101-0588-01L.series-metadata.json' AND parent = 3263;
+
+SELECT * FROM metadata WHERE json LIKE 'ey%';
+
+-- Create a table of all the diff records that are correctly set to not deprecated
+CREATE TABLE temp AS WITH LatestRecord AS (
+    SELECT URL, parent, MAX(DATETIME(found)) AS latest_found
+    FROM metadata
+    WHERE record_type = 1
+    GROUP BY URL, parent
+)
+SELECT t.key
+FROM metadata t
+JOIN LatestRecord lr ON t.URL = lr.URL AND t.found = lr.latest_found AND t.parent = lr.parent
+WHERE t.record_type = 1 AND deprecated = 0; -- 2287
+
+
+DROP TABLE IF EXISTS temp;
+
+SELECT COUNT(*) FROM temp;
+
+-- Get the records which are initial only in the db and are also not deprecated
+INSERT INTO temp SELECT key FROM metadata WHERE deprecated = 0 AND record_type = 0 AND key IN (SELECT key FROM metadata GROUP BY parent, URL HAVING COUNT(*) = 1); -- 2207
+
+-- Get the records which are final in the db, are in a group of at least three and are not deprecated
+INSERT INTO temp SELECT key FROM metadata WHERE deprecated = 0 AND record_type = 2 AND URL IN (SELECT URL FROM metadata GROUP BY parent, URL HAVING COUNT(*) > 2) AND parent IN (SELECT parent FROM metadata GROUP BY parent, URL HAVING COUNT(*) > 2);
+SELECT key FROM metadata WHERE deprecated = 0 AND record_type = 2 AND URL IN (SELECT URL FROM metadata GROUP BY parent, URL HAVING COUNT(*) > 2) AND parent IN (SELECT parent FROM metadata GROUP BY parent, URL HAVING COUNT(*) > 2);
+
+SELECT COUNT(*) FROM (SELECT COUNT(*) FROM metadata GROUP BY URL, parent HAVING COUNT(*) = 1);
+SELECT COUNT(*) FROM (SELECT COUNT(*) FROM metadata GROUP BY URL, parent HAVING COUNT(*) > 1);
+
+SELECT URL, parent FROM (SELECT COUNT(*), * FROM metadata GROUP BY URL, parent HAVING COUNT(*) = 1);
+SELECT URL, parent FROM (SELECT COUNT(*), * FROM metadata GROUP BY URL, parent HAVING COUNT(*) > 1);
+
+SELECT * FROM metadata;
