334626176187f6abb0ba2c504ad8e1c47376fb58
AliSot2000 <alisot200@gmail.com>
2024-09-23 18:35:25 +0200

    + added stronger assert

--- src/eth_loader/stream_loader.py
+++ src/eth_loader/stream_loader.py
@@ -743,14 +743,14 @@ class BetterStreamLoader(BaseSQliteDB):
                            f"AND found = '{now}'"
                            f"AND (record_type IS NULL OR record_type = 0)")
 
-        result = self.sq_cur.fetchone()
-        assert result is not None, "Just inserted the bloody thing"
+        result = self.sq_cur.fetchall()
+        assert len(results) == 1, f"Just inserted the bloody thing found {len(results)}"
 
         # Add entry into the assoz table
         self.debug_execute(f"INSERT OR IGNORE INTO metadata_episode_assoz (metadata_key, episode_key) "
-                           f"VALUES ({parent_id}, {result[0]})")
+                           f"VALUES ({parent_id}, {result[0][0]})")
 
-        return result[0]
+        return result[0][0]
 
     def link_episode_streams(self, episode_id: int, streams: List[int]):
         """
