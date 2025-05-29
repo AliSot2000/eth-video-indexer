751afa7ff55760b8d22249d17051519da56f06ca
AliSot2000 <alisot200@gmail.com>
2025-05-29 16:12:39 +0200

    + More excludes

--- scripts/pull_from_server.sh
+++ scripts/pull_from_server.sh
@@ -3,5 +3,7 @@
 rsync -avu --stats --progress --exclude="*.bak" \
   --exclude="lin_venv/" \
   --exclude="*__pycache__/*" \
+  --exclude=".git/*"
+  --exclude=".idea/*"
   ubuntu-home-lab:/home/oicunt/eth-video-indexer/ \
   ../../eth-video-indexer/
